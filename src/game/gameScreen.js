cba7572c2f63e8bc400e7a398085bd85473914d5
Inikon <ZhanZS@mail.ru>
2020-12-27 19:56:49 +0300

    fix: delete unused code

--- src/game/gameScreen.js
+++ src/game/gameScreen.js
@@ -10,67 +10,6 @@ const Timer = require('./core/timer.js');
 const Team = require('./core/team.js');
 const Game = require('./core/game.js');
 const Player = require('./core/player.js');
-//let bullets = [];
-
-
-
-
-/*class Player{
-  constructor(name, health, pos, color){
-    this.name = name;
-    this.health = health;
-    this.weapons = [new Weapon(10)];
-    this.currentWeapon = this.weapons[0];
-    this.angle = 0;
-
-    this.graphic = new GraphicPlayer(pos, 10, color);
-    this.target = new GraphicPoint(pos, 5, color);
-    
-  }
-
-  hurt(damage){
-    this.health -= damage;
-    if (this.health <= 0 ){
-      this.health = 0;
-      this.onKilled();
-    }
-  }
-
-  cure(damage){
-    this.health += damage;
-  }
-
-  shot(bullets){
-    let direction = new Vector(Math.cos(this.angle / 30), Math.sin(this.angle / 30));
-    if (this.currentWeapon){
-      this.currentWeapon.shot(bullets, this.graphic.position, direction);
-      this.onShot && this.onShot();
-    }
-  }
-
-  react(bullets, deltaTime){
-    bullets.forEach(it=>{
-      if (it.graphic.position.clone().sub(this.graphic.position).abs()<10){
-        if (!it.isDeleted){
-          it.isDeleted = true;
-          this.hurt(70);
-        }
-      }
-    });  
-  }
-
-  render(context, deltaTime){
-    this.target.position = new Vector(Math.cos(this.angle / 30), Math.sin(this.angle / 30)).scale(100).add(this.graphic.position)
-    this.graphic.render(context, deltaTime, {health:this.health});
-    
-    if (this.isActive){
-      this.target.render(context, deltaTime);
-    }
-  }
-}
-
-*/
-
 
 
 function newGame(){
@@ -86,7 +25,6 @@ function newGame(){
   }
   return game;
 }
-
 
 
 class GameScreen extends Control{
