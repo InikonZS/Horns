0be7f5c0f0cded4e9eb7cd42082fa05c33f4f412
Inikon <ZhanZS@mail.ru>
2021-01-02 17:37:07 +0300

    fix: many page, page maneger

--- src/application/application.js
+++ src/application/application.js
@@ -1,15 +1,25 @@
 const Control = require('common/control.js');
 const PageManager = require('./pageManager.js');
 const GameScreen = require('game/gameScreen.js');
+const AboutScreen = require('./about/aboutPage.js');
+const AnotherScreen = require('./another/anotherPage.js');
 
 class Application extends Control{
   constructor(parentNode, config){
     super(parentNode);
     // this.pageManager = new PageManager(parentNode);
     this.logo = new Control(this.node, 'div', 'logo', 'Horns');
-    this.menu = new Control(this.node, 'div', 'navi', 'типо меню навигации Игра / About');
+    //this.menu = new Control(this.node, 'div', 'navi', 'типо меню навигации Игра / About');
     //const gamePage = new GamePage();
+    this.pageManager = new PageManager(this.node, 'navi');
     this.gameScreen = new GameScreen(this.node);
+    this.pageManager.add(this.gameScreen, 'Game');
+    this.aboutScreen = new AboutScreen(this.node);
+    this.pageManager.add(this.aboutScreen, 'About');
+    this.anotherScreen = new AnotherScreen(this.node);
+    this.pageManager.add(this.anotherScreen, 'Something');
+    this.pageManager.select(0);
+
     this.footer = new Control(this.node, 'div', 'footer', 'типо футер, наши гитхабы');
     
   }
--- src/application/pageManager.js
+++ src/application/pageManager.js
@@ -3,13 +3,47 @@ const AboutPage = require('./about/aboutPage.js');
 const GamePage = require('./game/gamePage.js');
 
 class PageManager extends Control{
-  constructor(parentNode, config){
-    super(parentNode);
-    this.logo = new Control(this.node, 'h1', 'logo', 'Logo');
-    this.menu = new Control(this.node, 'div', 'navi', 'типо меню навигации Игра / About');
-    const gamePage = new GamePage();
-    this.footer = new Control(this.node, 'div', 'footer', 'типо футер, наши гитхабы');
+  constructor (parentNode, className){
+    super(parentNode, 'div', className);
+    this.pages = [];
+    this.marks = [];
+    this.currentPage = null;
+    this.currentIndex = -1;
+  }
+
+  add(page, name){
+    page.hide();
+    this.pages.push(page);
+    let mark = new Control(this.node, 'div', 'navi_item', name);
+    mark.node.addEventListener('click', ()=>{
+      this.select(this.pages.indexOf(page));
+    });
+    this.marks.push(mark);
+  }
+
+  selectByName(name){
+    let index = this.pages.findIndex(it=>it.name == name);
+    this.select(index);
+  }
+
+  selectByScene(page){
+    let index = this.pages.indexOf(page);
+    this.select(index);
+  }
+
+  select(index){
+    this.pages.forEach((it, i)=>{
+      if (i!=index){
+        it.hide();
+      } else {
+        it.show();
+        this.currentPage = it;
+        this.currentIndex = i;
+      }
+    });
   }
 }
+
+module.exports = PageManager;
 
 module.exports = PageManager;
