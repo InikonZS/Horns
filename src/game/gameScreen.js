cdf5a547d8d7657ce7d2a79ceae0feeab8f5cd91
General <general-m7@yandex.ru>
2021-01-10 06:06:23 +0300

    feat:add weaponScreen

--- src/game/gameScreen.js
+++ src/game/gameScreen.js
@@ -47,7 +47,7 @@ class GameScreen extends Control {
     }
 
     this.playPanel = new PlayPanel(this.panel.node);
-    this.playPanel.weaponMenu.onSelect = index => {
+    this.playPanel.openWeapon.onSelect = index => {
       this.game.currentTeam.currentPlayer.setWeapon(index);
     }
 
@@ -59,7 +59,7 @@ class GameScreen extends Control {
       this.panel.selectByScene(this.playPanel);
       this.game = newGame();
       this.game.onNext = (player) => {
-        this.playPanel.weaponMenu.select(player.weapons.indexOf(player.currentWeapon), true);
+        this.playPanel.openWeapon.select(player.weapons.indexOf(player.currentWeapon), true);
         this.playPanel.windIndicator.node.textContent = this.game.wind.toFixed(2);
       }
       this.playPanel.teamIndicator.clear();
@@ -84,6 +84,14 @@ class GameScreen extends Control {
     this.panel.add(this.editor);
     this.menu.onEditor = () => {
       this.panel.selectByScene(this.editor);
+    }
+
+    this.editor.onExit = () => {
+      this.panel.selectByScene(this.menu);
+    }
+
+    this.settings.onExit = () => {
+      this.panel.selectByScene(this.menu);
     }
 
     this.panel.selectByScene(this.preloader);
@@ -118,12 +126,7 @@ class GameScreen extends Control {
     window.addEventListener('resize', () => {
       this.autoSize();
     })
-    // body.addEventListener('click', (ev) => {
-    //   if (ev.target.classlist.contains('weapon_btn')) {
-    //     console.log(ev.target);
-    //   }
 
-    // })
   }
 
   autoSize() {
--- src/game/settingsMenu.js
+++ src/game/settingsMenu.js
@@ -32,9 +32,10 @@ class SettingsMenu extends Control {
 
         let settingsItemTeamChoice = new Control(settingsWrapper.node, 'div', 'settings_item settings_team_selection');
         this.itemTitle = new Control(settingsItemTeamChoice.node, 'div', 'settings_item_title', 'Казарма');
-        let settingsTeamTable = new Control(settingsItemTeamChoice.node, 'div', 'team-table');
-        this.teamTable = new Control(settingsTeamTable.node, 'table', '');
-        let tBody = new Control(this.teamTable.node, 'tbody', '');
+        let settingsTeamChoiceInner = new Control(settingsItemTeamChoice.node, 'div', 'settings_team_inner');
+        this.teamNameChoice = new Control(settingsTeamChoiceInner.node, 'div', 'settings_team_block team_name', 'Команда');
+        this.teamPoints = new Control(settingsTeamChoiceInner.node, 'div', 'settings_team_block team_status', 'Очки');
+
 
         let settingsItemChart = new Control(settingsWrapper.node, 'div', 'settings_item settings_chart');
         this.itemTitle = new Control(settingsItemChart.node, 'div', 'settings_item_title', 'Настройки схемы');
@@ -51,9 +52,11 @@ class SettingsMenu extends Control {
         this.chartOption = new Control(this.chartSelect.node, 'option', '', 'Армагедон');
 
         this.startGame = new Control(settingsWrapper.node, 'div', 'startGame_btn', 'Начать игру');
-        this.exit = new Control(settingsWrapper.node, 'div', 'exit_btn', 'Выход');
+        this.return = new Control(settingsWrapper.node, 'div', 'return_btn', 'Назад');
 
-
+        this.return.node.onclick = () => {
+            this.onreturn && this.onreturn();
+        }
     }
 }
 
